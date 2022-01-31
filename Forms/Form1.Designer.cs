975955929f3ee6c91913b6f6074fa20c1a8092a7
Shan15Dev <shansai@bluewin.ch>
2022-01-31 10:59:11 +0100

    Minimum window size added

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.chromium = new System.Windows.Forms.Panel();
             this.urlField = new MetroFramework.Controls.MetroTextBox();
             this.search_btn = new MetroFramework.Controls.MetroButton();
@@ -41,79 +42,56 @@
             // 
             // chromium
             // 
-            this.chromium.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-            this.chromium.Location = new System.Drawing.Point(0, 67);
+            resources.ApplyResources(this.chromium, "chromium");
             this.chromium.Name = "chromium";
-            this.chromium.Size = new System.Drawing.Size(800, 385);
-            this.chromium.TabIndex = 2;
             this.chromium.Paint += new System.Windows.Forms.PaintEventHandler(this.chromium_Paint);
             // 
             // urlField
             // 
-            this.urlField.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-            this.urlField.Location = new System.Drawing.Point(85, 38);
+            resources.ApplyResources(this.urlField, "urlField");
             this.urlField.Name = "urlField";
-            this.urlField.Size = new System.Drawing.Size(625, 23);
-            this.urlField.TabIndex = 3;
             this.urlField.KeyUp += new System.Windows.Forms.KeyEventHandler(this.urlField_KeyUp);
             // 
             // search_btn
             // 
-            this.search_btn.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.search_btn.Location = new System.Drawing.Point(716, 38);
+            resources.ApplyResources(this.search_btn, "search_btn");
             this.search_btn.Name = "search_btn";
-            this.search_btn.Size = new System.Drawing.Size(75, 23);
-            this.search_btn.TabIndex = 4;
-            this.search_btn.Text = "Search";
             this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
             // 
             // forward_btn
             // 
-            this.forward_btn.Location = new System.Drawing.Point(55, 38);
+            resources.ApplyResources(this.forward_btn, "forward_btn");
             this.forward_btn.Name = "forward_btn";
-            this.forward_btn.Size = new System.Drawing.Size(24, 23);
-            this.forward_btn.TabIndex = 5;
-            this.forward_btn.Text = "->";
             this.forward_btn.Click += new System.EventHandler(this.forward_btn_Click);
             // 
             // back_btn
             // 
-            this.back_btn.Location = new System.Drawing.Point(25, 38);
+            resources.ApplyResources(this.back_btn, "back_btn");
             this.back_btn.Name = "back_btn";
-            this.back_btn.Size = new System.Drawing.Size(24, 23);
-            this.back_btn.TabIndex = 6;
-            this.back_btn.Text = "<-";
             this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 7;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.settingsToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
             // 
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.settingsToolStripMenuItem.Text = "Settings";
-            this.settingsToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            resources.ApplyResources(this.settingsToolStripMenuItem, "settingsToolStripMenuItem");
             this.settingsToolStripMenuItem.Click += new System.EventHandler(this.einstellungenToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.back_btn);
             this.Controls.Add(this.forward_btn);
             this.Controls.Add(this.search_btn);
@@ -122,7 +100,6 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "ShanBrowser";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
